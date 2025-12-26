7e246cbf3c2e096f0443a0a40de34625565de86c
Jahnavi <gaurjahnavi84@gmail.com>
2025-12-26 14:39:21 +0530

    change 2

--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -1,22 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Tooltip = ({ text, children }) => {
   const [showTooltip, setShowTooltip] = useState(false);
 
-  return (
-    <div 
-      className="tooltip" 
-      onMouseEnter={() => setShowTooltip(true)} 
-      onMouseLeave={() => setShowTooltip(false)}
-      style={{ display: 'inline-block' }}
-    >
-      {/* This renders the children (h2, p, button, etc.) */}
-      {children}
-
-      {/* Conditional rendering for the tooltip text */}
-      {showTooltip && <div className="tooltiptext">{text}</div>}
-    </div>
-  );
+  return React.cloneElement(children, {
+    className: "tooltip",
+    onMouseEnter: () => setShowTooltip(true),
+    onMouseLeave: () => setShowTooltip(false),
+    children: (
+      <>
+        {children.props.children}
+        {showTooltip && <div>{text}</div>}
+      </>
+    )
+  });
 };
 
 export default Tooltip;
