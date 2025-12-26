165ab218cc252a86e72bebb85266b1b65195ec91
Jahnavi <gaurjahnavi84@gmail.com>
2025-12-26 18:42:17 +0530

    last change

--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -1,14 +1,16 @@
-import React, { useState } from "react";
-
 const Tooltip = ({ text, children }) => {
   const [showTooltip, setShowTooltip] = useState(false);
 
   return (
-    <div onMouseEnter={() => setShowTooltip(true)} onMouseLeave={() => setShowTooltip(false)}>
+    <span
+      onMouseEnter={() => setShowTooltip(true)}
+      onMouseLeave={() => setShowTooltip(false)}
+      style={{ position: "relative" }}
+    >
       {children}
-      {showTooltip && <div className="tooltiptext">{text}</div>}
-    </div>
+      {showTooltip && (
+        <span className="tooltiptext">{text}</span>
+      )}
+    </span>
   );
 };
-
-export default Tooltip;
